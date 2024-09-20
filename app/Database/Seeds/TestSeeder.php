2215fc1b44643e96397a54d35cde07e946035de7
Ángel Blanco Rodríguez <73697059+AngelBlanco97@users.noreply.github.com>
2024-09-20 20:56:00 +0200

    [perf] Create a seeder to invoke the others ones

---
+++ app/Database/Seeds/TestSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class TestSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call('ClientSeeder');
+        $this->call('BedroomsType');
+        $this->call('BedroomsSeeder');
+        $this->call('BedroomPrices');
+    }
+}
